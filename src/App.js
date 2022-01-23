53e3f6bb773c327b7043accb783cbf8c26c466c8
atf1997 <96771719+atf1997@users.noreply.github.com>
2022-01-23 11:57:09 +0200

    featL fetch data from internet

--- src/App.js
+++ src/App.js
@@ -1,44 +1,24 @@
+import { useReducer, useState, useEffect } from "react";
 import "./App.css";
-import logo from"./logo.svg";
 
-const dishes = ["Madroña", "Salmon"];
-const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
+function App({ user }) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${user}`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
 
-function Header(props) {
+  if (user != null)
+    return (
+      <>
+        <h1>{JSON.stringify(data)}</h1>
+      </>
+    );
   return (
-    <header>
-      <h1> {props.name}'s Kitchen</h1>
-    </header>
-  );
-}
-
-function Main(props) {
-  return (
-    <section>
-      <ul>
-        {props.dishes.map((dishObject) => (
-          <ul key={dishObject.id}>{dishObject.title}</ul>
-        ))}
-      </ul>
-      <img src={logo} height={200} alt="image of dish" />
-    </section>
-  );
-}
-function Footer(params) {
-  return (
-    <footer>
-      <p>Copyright {params.year}</p>
-    </footer>
-  );
-}
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Abdo" />
-      <Main dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
+    <>
+      <h1>No data</h1>
+    </>
   );
 }
 
